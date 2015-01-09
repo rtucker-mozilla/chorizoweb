c9a6aabe7db9f375974cc050e4819fc69e675954
Rob Tucker <rtucker@mozilla.com>
2015-01-09 09:35:35 -0500

    Update to run_wsgi.py

--- run_wsgi.py
+++ run_wsgi.py
@@ -1,8 +1,14 @@
+import sys, os
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(PROJECT_DIR)
+activate_this = os.path.join(PROJECT_DIR, 'venv', 'bin', 'activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
 from moz_au_web.app import create_app
 from moz_au_web.settings import ProdConfig
-import os
 
 app = create_app(ProdConfig)
+application = app
 
 if __name__ == "__main__":
     app.debug = False
