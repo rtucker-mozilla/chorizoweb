713e901437a3c1c4acb4a102771736a8a1ca74bd
Rob Tucker <rtucker@mozilla.com>
2015-06-04 07:02:58 -0400

    Fixes to tests to use UpdateGroup model

--- test_chorizo_queue_state.py
+++ test_chorizo_queue_state.py
@@ -164,6 +164,8 @@ class testChorizoQueueState(unittest.TestCase):
     	group1.group_name = group_name
     	self.cqs.add_host_to_group_if_not_exists(group1, host_name)
     	self.cqs.add_host_to_group_if_not_exists(group1, host_name2)
+    	next_host = self.cqs.get_next_host_by_group(group1)
+    	self.assertFalse(next_host == False)
     	#self.assertEqual(self.cqs.get_next_host_by_group(group_name), host_name)
 if __name__ == '__main__':
     unittest.main()
